id-Software/Quake-III-Arena

commit 96f94a2891eeaa4b486de2c174ed8c00fcf0d271

Fix compiling GLSL shaders under Windows.
Now the `sed` command cope with files using Windows-style line endings.

Makefile
@@ -1183,7 +1183,7 @@ define DO_REF_STR
 $(echo_cmd) "REF_STR $<"
 $(Q)rm -f $@
 $(Q)echo "const char *fallbackShader_$(notdir $(basename $<)) =" >> $@
-$(Q)cat $< | sed 's/^/\"/;s/$$/\\n\"/' >> $@
+$(Q)cat $< | sed -e 's/^/\"/;s/$$/\\n\"/' -e 's/\r//g' >> $@
 $(Q)echo ";" >> $@
 endef
 

mickael9/ioq3

commit 7d098d9bf88f424e808f563d701a4f64f37bf304

commit https://github.com/mickael9/ioq3/commit/96f94a2891eeaa4b486de2c174ed8c00fcf0d271 broke support on OpenBSD for me.  quick fix by only using extra line endings check when platform is mingw32

Makefile
@@ -1125,11 +1125,18 @@ $(echo_cmd) "REF_CC $<"
 $(Q)$(CC) $(SHLIBCFLAGS) $(CFLAGS) $(CLIENT_CFLAGS) $(OPTIMIZE) -o $@ -c $<
 endef
 
+ifeq ($(PLATFORM),mingw32)
+GLSL_SED_ARGS=-e 's/^/\"/;s/$$/\\n\"/' -e 's/\r//g'
+else
+GLSL_SED_ARGS='s/^/\"/;s/$$/\\n\"/'
+endif
+
 define DO_REF_STR
 $(echo_cmd) "REF_STR $<"
 $(Q)rm -f $@
 $(Q)echo "const char *fallbackShader_$(notdir $(basename $<)) =" >> $@
-$(Q)cat $< | sed -e 's/^/\"/;s/$$/\\n\"/' -e 's/\r//g' >> $@
+$(Q)cat $< | sed $(GLSL_SED_ARGS) >> $@
+
 $(Q)echo ";" >> $@
 endef
 